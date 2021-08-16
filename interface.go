0292ea49d4ea4f21ccd0040a3c27ac43572470be
Leandro Franciscato <leandro_franciscato@hotmail.com>
2021-08-16 17:13:08 -0300

    testing new aproach on consumers

--- interface.go
+++ interface.go
@@ -2,6 +2,6 @@ package kafka
 
 type IKafka interface {
 	Produce(topic string, message []byte, erro error) error
-	Consume(topic, groupName string) (msgChannel chan []byte, err error)
-	ConsumeBulk(topic, groupName string, maxBufferSize, numberOfRoutines int) (msgChannel chan []byte, err error)
+	Consume(topic, groupName string) (msgChannel []chan []byte, err error)
+	ConsumeBulk(topic, groupName string, maxBufferSize, numberOfRoutines int) (msgChannel []chan []byte, err error)
 }
